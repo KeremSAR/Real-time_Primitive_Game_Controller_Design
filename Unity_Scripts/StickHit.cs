093f668e6ef4a6db95800b69794535a6a4bf19ea
Kerem Sarı <98567140+KeremSAR@users.noreply.github.com>
2022-07-06 21:01:41 +0300

    Add files via upload

---
+++ Unity_Scripts/StickHit.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickHit : MonoBehaviour
+{
+    public float offset;
+    private BoxCollider _boxCollider;
+    void Start()
+    {
+        _boxCollider = this.gameObject.GetComponent<BoxCollider>();
+    }
+    
+    
+    private static float WrapAngle(float angle)
+    {
+        angle%=360;
+        if(angle >180)
+            return angle - 360;
+ 
+        return angle;
+    }
+    // do the operation when hits the object
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "spawnObj")
+        {
+            float rotZ = this.gameObject.transform.rotation.eulerAngles.z;
+            float ObjrotZ = other.gameObject.transform.rotation.z;
+            Debug.Log("objrot değeri " +(ObjrotZ));
+            Debug.Log("stick rot değeri" +(WrapAngle(rotZ)));
+            if ((ObjrotZ-offset<=WrapAngle(rotZ))&&(WrapAngle(rotZ)<=ObjrotZ+offset))
+            {
+              //  other.gameObject.SetActive(false);
+            }
+            
+        }
+      
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity_Scripts/networkHost.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public static class Extensions
+{
+	
+    public static string Filter(this string str, List<char> charsToRemove)
+    {
+        charsToRemove.ForEach(c => str = str.Replace(c.ToString(), String.Empty));
+        return str;
+    }
+    public static string FilterString(this string str, List<string> stringsToRemove)
+    {
+        stringsToRemove.ForEach(c => str = str.Replace(c.ToString(), String.Empty));
+        return str;
+    }
+}
+public class networkHost : MonoBehaviour
+{
+    public string ip;
+    public IPAddress hostaddress;
+ 
+    public int port;
+    public IPEndPoint ipep;
+    public IPEndPoint sender;
+    public UdpClient newSock;
+    public Thread networkThread;
+ 
+    public byte[] receivedData;
+    public string dataString;
+    public string[] EulerAngles;
+    public InputField ip_text;
+    public InputField port_text;
+    private Quaternion quatern;
+    private Quaternion inverseQt;
+    void Start()
+    {
+        inverseQt = Quaternion.identity;
+        networkThread = new Thread(ReceiveData);   //  UDP network thread
+        networkThread.Start();
+        
+        receivedData = new byte[0];
+        
+        ipep = new IPEndPoint(IPAddress.Parse(ip), port);
+        newSock = new UdpClient(ipep);
+       // sender = new IPEndPoint(IPAddress.Any, 9998);*/
+    }
+
+    public void IPtext()
+    {
+        ip = ip_text.text; 
+    }
+
+    public void PortText()
+    {
+        
+        port = int.Parse(port_text.text);
+        Debug.Log("port num %d" + port);
+        
+    }
+    void Update()
+    {
+        
+        ReceiveData();
+       // StartCoroutine(Delay());
+        
+    } 
+  
+    private string[] parseval(string data_string)
+    {
+        
+        string[] data_values = data_string.Replace(".",",").Split(' ');
+        foreach (string val in data_values)
+        {
+            Debug.Log("value parsed " + val);
+        }
+        return data_values;
+    }
+    public void ReceiveData()
+    {
+        try
+        {
+            if (newSock.Available > 0)
+            {
+                receivedData = newSock.Receive(ref sender);
+
+                dataString = Encoding.ASCII.GetString(receivedData);
+                EulerAngles = dataString.Replace(".",",").Split(' ');;
+
+                //quatern = new Quaternion(float.Parse(EulerAngles[0]), 0, 0, float.Parse(EulerAngles[3]));
+               // quatern = new Quaternion(0, float.Parse(EulerAngles[1]), 0, float.Parse(EulerAngles[3]));
+               //quatern = new Quaternion(0, 0,float.Parse(EulerAngles[2]) , float.Parse(EulerAngles[3]));
+                quatern = new Quaternion(float.Parse(EulerAngles[0]), float.Parse(EulerAngles[2]), -1*float.Parse(EulerAngles[1]), float.Parse(EulerAngles[3]));
+              
+                
+                this.transform.rotation = quatern * inverseQt;//Quaternion.Euler(/*-1f*float.Parse(EulerAngles[0].Replace(".",","))*/0,/*float.Parse(EulerAngles[1].Replace(".",","))*/0,float.Parse(EulerAngles[1]));
+                
+
+            }
+            else
+            {
+                // print("No data to receive");
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log("waiting the socket connetion");
+        }
+    }
+    public void resetRotation()
+    {
+        inverseQt = Quaternion.Inverse(quatern);
+    }
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(0.1f);
+        ReceiveData();
+    }
+}
